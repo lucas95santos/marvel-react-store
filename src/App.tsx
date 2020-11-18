ae271cdbaf3c83553a3e4ecd671084993e65bb36
Lucas Santos <lucasa95santos@gmail.com>
2020-11-18 14:10:16 -0400

    chore: configurando eslint e prettier no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Marvel Store</h1>
-  );
-}
+const App: React.FunctionComponent = () => {
+  return <h1>Marvel Store</h1>;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,4 @@ import App from './App';
 // styles
 import './styles/global.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
